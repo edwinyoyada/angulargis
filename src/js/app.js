ac7a27349c8da47d48ca5c13dfa0ca1e4363cc07
Edwin yoyada Pratama <Edwin yoyada Pratama>
2016-03-08 17:57:30 +0700

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+var gisApp = angular.module('gisApp', ['uiGmapgoogle-maps']);
+
+gisApp.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        // v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+});
---
+++ src/js/controller.js
@@ -0,0 +1,12 @@
+gisApp.controller("firstController", function($scope, uiGmapGoogleMapApi) {
+    // Do stuff with your $scope.
+    // Note: Some of the directives require at least something to be defined originally!
+    // e.g. $scope.markers = []
+
+    // uiGmapGoogleMapApi is a promise.
+    // The "then" callback function provides the google.maps object.
+    uiGmapGoogleMapApi.then(function(maps) {
+    	$scope.map = { center: { latitude: -2.3163654, longitude: 119.0851044 }, zoom: 6 };
+    	$scope.marker = adiraData;
+    });
+});
