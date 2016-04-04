2c4665e297dacd01a11986a024f64fdeb6d229f3
Oki Setiawan <okisetiawan0101@gmail.com>
2016-04-04 23:21:13 +0700

    ganti url k server

--- src/js/factory.js
+++ src/js/factory.js
@@ -1,6 +1,6 @@
-var BackendUrl ='localhost:8000/';
+//var BackendUrl ='localhost:8000/';
 
-//var BackendUrl = window.location.href;
+var BackendUrl = '128.199.119.162:8000';
 
 gisApp.factory('OrganizationType', function($resource) {
 	return $resource(BackendUrl+'api/organization_types');
